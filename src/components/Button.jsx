58b3ff10abeb3500d065371f42790352bad23635
Dildora Muslimova <faithpacifica@yahoo.com>
2023-12-28 07:57:51 +0900

    create subscribe and footer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,15 @@
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL,backgroundColor,borderColor,textColor,fullWidth}) => {
   return (
-    <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full font-montserrat px-7 bg-coral-red border-coral-red">
+    <button className={`flex items-center justify-center gap-2 py-4 text-lg leading-none border font-montserrat px-7 rounded-full
+    ${
+      backgroundColor
+     ?  `${backgroundColor} ${textColor} ${borderColor}`
+     :
+    'bg-coral-red border-coral-red text-white'} rounded-full ${fullWidth && "w-full"}`}>
+      
     {label}
+    
    {iconURL &&  <img 
     src={iconURL}
     alt="arrow right icon"
